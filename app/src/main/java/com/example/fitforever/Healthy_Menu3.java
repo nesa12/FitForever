21251326d42920daad22e752a614ad47f7c49ee9
nesa12 <87229466+nesa12@users.noreply.github.com>
2021-09-26 22:44:51 +0530

    2nd new files added

---
+++ app/src/main/java/com/example/fitforever/Healthy_Menu3.java
@@ -0,0 +1,14 @@
+package com.example.fitforever;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Healthy_Menu3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_healthy__menu3);
+    }
+}
